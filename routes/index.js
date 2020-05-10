7a160c3d2cc89f4074f862bbb2efe429995e2d31
ahmed_elbassel <elbassel.n13@gmail.com>
2020-05-11 01:14:15 +0200

    Changing return text

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express Bassel' });
+  res.render('index', { title: 'Express Bassel change text' });
 });
 
 module.exports = router;
